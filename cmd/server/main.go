087bcd8949879ef48ca1f711dc7bb9a0b7c38b71
thisantm <thiago.sant.mach@gmail.com>
2025-01-22 13:14:13 -0300

    Add RabbitMQ connection and signal handling to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const connectionString string = "amqp://guest:guest@localhost:5672/"
+
+	amqpConnection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer amqpConnection.Close()
+
+	log.Println("Connection succeeded to RabbitMQ")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	log.Println("Ending connection")
 }
