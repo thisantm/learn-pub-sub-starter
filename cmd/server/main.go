19acf1db104e7b33682bb49cfa7831402c5c0846
thisantm <thiago.sant.mach@gmail.com>
2025-01-22 14:45:21 -0300

    Add JSON publishing of playing state to RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,8 +22,25 @@ func main() {
 		log.Fatal(err)
 	}
 	defer amqpConnection.Close()
-
 	log.Println("Connection succeeded to RabbitMQ")
+
+	amqpChannel, err := amqpConnection.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer amqpChannel.Close()
+
+	playingState, err := json.Marshal(routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pubsub.PublishJSON(amqpChannel, routing.ExchangePerilDirect, routing.PauseKey, playingState)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
